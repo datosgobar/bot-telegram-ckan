3a8d1ddccedd7407675cecdeefa5915af8b3868f
Alejandra <maria.alejandra.fauquie@gmail.com>
2025-07-10 15:06:57 -0300

    idem

--- utils.py
+++ utils.py
@@ -15,10 +15,10 @@ def write_json(file_path, dict_data):
         json.dump(dict_data, f, indent=4, ensure_ascii=False)
 
 
-def new_data_message(update_df):
+def new_data_message(update_df,org_dict):
     """Elige el texto a usar de acuerdo a la cantidad de datasets nuevos"""
     if len(update_df) == 1:
-        text = text_one_dataset(update_df)
+        text = text_one_dataset(update_df, org_dict)
     else:
         text = text_sev_dataset(update_df)
     return text
@@ -28,9 +28,10 @@ def new_org_message(org_updates, org_inter, ckan_portal):
     """Elige el texto a usar si hay nuevos nodos"""
 
     if len(org_inter) == 1:
-        alias = next((k for k, v in org_updates.items() if v == org_inter[0]), None)
+        #alias = next((k for k, v in org_updates.items() if v == org_inter[0]), None)
+        alias = org_inter[0]
         org_url = ckan_portal + "dataset?organization=" + alias
-        text = text_one_org(org_inter, org_url)
+        text = text_one_org(alias, org_url, org_updates)
     else:
         text = text_sev_orgs(org_inter, org_updates, ckan_portal)
 
